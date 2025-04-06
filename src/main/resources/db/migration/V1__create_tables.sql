641c2fa40af3fb00f3182451c5ef0b2fe18c9b9c
Andrew da Silva <andrewairamdasilva@gmail.com>
2025-04-06 17:26:53 -0300

    fix: fix card user id

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tb_users (
-    user_id UUID PRIMARY KEY,
+    user_id VARCHAR(36) PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -8,10 +8,10 @@ CREATE TABLE tb_users (
 );
 
 CREATE TABLE tb_accounts (
-    account_id UUID PRIMARY KEY,
+    account_id VARCHAR(36) PRIMARY KEY,
     account_number VARCHAR(8) UNIQUE NOT NULL,
     agency VARCHAR(4) NOT NULL,
-    user_id UUID NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
     is_active BOOLEAN DEFAULT TRUE NOT NULL,
     balance DECIMAL(15,2) DEFAULT 0.00 NOT NULL,
     credit_limit DECIMAL(15,2) DEFAULT 0.00 NOT NULL,
@@ -22,9 +22,9 @@ CREATE TABLE tb_accounts (
     CONSTRAINT fk_account_user FOREIGN KEY (user_id) REFERENCES tb_users(user_id) ON DELETE CASCADE
 );
 CREATE TABLE tb_cards (
-    card_id UUID PRIMARY KEY,
-    user_id UUID NOT NULL,
-    account_id UUID NOT NULL,
+    card_id VARCHAR(36) PRIMARY KEY,
+    user_id VARCHAR(36) NOT NULL,
+    account_id VARCHAR(36) NOT NULL,
     card_number VARCHAR(8) UNIQUE NOT NULL,
     card_holder_name VARCHAR(255) NOT NULL,
     cvv VARCHAR(3) NOT NULL,
@@ -37,9 +37,9 @@ CREATE TABLE tb_cards (
     CONSTRAINT fk_card_account FOREIGN KEY (account_id) REFERENCES tb_accounts(account_id) ON DELETE CASCADE
 );
 CREATE TABLE tb_transactions (
-    transaction_id UUID PRIMARY KEY,
-    payer_id UUID NULL,
-    payee_id UUID NULL,
+    transaction_id VARCHAR(36) PRIMARY KEY,
+    payer_id VARCHAR(36) NULL,
+    payee_id VARCHAR(36) NULL,
     amount DECIMAL(15,2) NOT NULL,
     transaction_status VARCHAR(50) NOT NULL,
     transaction_type VARCHAR(50) NOT NULL,
